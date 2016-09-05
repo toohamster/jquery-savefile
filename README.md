1b5bddb11429f4bf7dd092cb4711765019bc7d7c
toohamster <449211678@qq.com>
2016-09-05 10:51:31 +0800

    import code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # jquery-savefile
 Save file in Browser
+
+```
+$.savefile.url(url, filename);
+// OR
+$.savefile.file(mimetype, filename, data);
+```
---
+++ jquery.savefile.js
@@ -0,0 +1,40 @@
+/**
+ * jQuery file download v0.1
+ *
+ * Save file in Browser 
+ * 
+ * https://github.com/toohamster/jquery-savefile
+ *
+ * @author toohamster, 2016
+ * @license MIT License <http://opensource.org/licenses/MIT>
+ */
+(function($) {
+    'use strict';
+
+    function saveAs(uri, filename) {
+        var link = document.createElement('a');
+        if (typeof link.download === 'string') {
+            document.body.appendChild(link); // Firefox requires the link to be in the body
+            link.download = filename;
+            link.href = uri;
+            link.click();
+            document.body.removeChild(link); // remove the link when done
+        } else {
+            location.replace(uri);
+        }
+    }
+
+    $.savefile = {
+        url: function(url, filename)
+        {
+            saveAs(url, filename);
+        },
+        file: function(mimetype, filename, data)
+        {
+            var data = 'data:' + mimetype + ';base64,' + window.btoa( unescape(encodeURIComponent(data)) );
+            saveAs(data, filename);
+        }
+    };
+    
+
+}(jQuery));
